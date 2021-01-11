f1ddab62e263fc65602626ea3d89d7738f089f1d
Soham Patel <soham.patel105876@marwadiuniversity.ac.in>
2021-01-11 14:44:51 +0530

    Validator in request method

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\TodoCreateRequest;
+use Illuminate\Support\Facades\Validator;
+
 use App\Models\Todo;
 
 class TodoController extends Controller
@@ -20,8 +23,10 @@ class TodoController extends Controller
     {
         return view('todos.edit');
     }
-    public function store(Request $request)
+    public function store(TodoCreateRequest $request)
     {
+        //validator is at -> /app/http/requests/TodoCreateRequest        
+
         Todo::create($request->all());
         return redirect()->back()->with('message','To-Do created');
     }
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -3,4 +3,14 @@
     @if(session()->has('message'))
     <div style="padding:10px;background-color:green;">{{session()->get('message')}}</div>
     @endif
+
+    @if ($errors->any())
+        <div style="padding:10px;background-color:red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 </div>
